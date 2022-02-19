9b373904f69359d2814b7b940e11511f71fea3e2
August <hello@aughost.codes>
2022-02-19 17:34:35 -0500

    Class-weekend-boring completed first round

--- projects/class-weekend-boring/js/main.js
+++ projects/class-weekend-boring/js/main.js
@@ -1,10 +1,18 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector('#check').addEventListener('click', check);
+let inputWindow = document.querySelector('#placeToSee');
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
 
   //Conditionals go here
 
+  if (day === 'tuesday' || day === 'thursday') {
+    inputWindow.textContent = 'CLASS DAY!';
+  } else if (day === 'saturday' || day === 'sunday') {
+    inputWindow.textContent = 'WEEKEND!';
+  } else {
+    inputWindow.textContent = 'Lame ass day.';
+  }
 
 }
