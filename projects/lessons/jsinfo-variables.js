3f31eb79aef46b32330227cc93ab59b707142d61
August <hello@aughost.codes>
2022-02-20 14:46:45 -0500

    JS Info variables lesson

---
+++ projects/lessons/jsinfo-variables.js
@@ -0,0 +1,11 @@
+let admin;
+let name = 'John';
+admin = name;
+alert(admin);
+
+let homePlanet = 'Earth';
+let currentVisitor = 'Bart Simpson';
+
+// Task 3 //
+const BIRTHDAY = '08.30.1988';
+// const age should not be caps //
