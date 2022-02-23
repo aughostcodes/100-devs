5a7628e6c3a98a017d7ed94af1e09b137097fa78
August <hello@aughost.codes>
2022-02-23 13:52:11 -0500

    JS Info functions tasks

--- projects/lessons/jsinfo-funcs.js
+++ projects/lessons/jsinfo-funcs.js
@@ -1,1 +1,19 @@
-console.log('connect');
+// Task 1 //
+// No difference between the two //
+
+
+// function checkAge(age) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         return confirm('Did parents allow you?');
+//     }
+// }
+
+function checkAge(age) {
+    return (age > 18) ? true : confirm('Did parents allow you?');
+}
+
+function checkAge(age) {
+    return (age > 18) || confirm('Did parents allow you?');
+}
