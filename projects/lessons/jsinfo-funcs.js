163bd8d6fa0e2ed323c27c6462b4882663c5727c
August <hello@aughost.codes>
2022-02-26 22:28:58 -0500

    JS Info functions power func

--- projects/lessons/jsinfo-funcs.js
+++ projects/lessons/jsinfo-funcs.js
@@ -17,3 +17,15 @@ function checkAge(age) {
 function checkAge(age) {
     return (age > 18) || confirm('Did parents allow you?');
 }
+
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+function pow(x, n) {
+    return x**n;
+}
