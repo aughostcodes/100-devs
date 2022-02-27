d78e5ed3f45581cf4685fbe1b6f3e4f2e2d6f1d0
August <hello@aughost.codes>
2022-02-27 13:35:44 -0500

    JS Info functions task complete

--- projects/lessons/jsinfo-funcs.js
+++ projects/lessons/jsinfo-funcs.js
@@ -19,11 +19,7 @@ function checkAge(age) {
 }
 
 function min(a, b) {
-    if (a < b) {
-        return a;
-    } else {
-        return b;
-    }
+    return ((a < b) ? a : b);
 }
 
 function pow(x, n) {
