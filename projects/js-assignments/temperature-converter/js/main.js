36a60c1fbcc6192bcea78d5b3c9e6b2623636f37
August <hello@aughost.codes>
2022-02-28 15:00:46 -0500

    Hungry

--- projects/js-assignments/temperature-converter/js/main.js
+++ projects/js-assignments/temperature-converter/js/main.js
@@ -6,27 +6,38 @@ const startingScale = document.getElementById('starting-scale');
 const endingScale = document.getElementById('ending-scale');
 let startingScaleValue;
 let endingScaleValue;
+let result;
 
 // Event listeners
 convertButton.addEventListener('click', convertTemp);
 
-// Get temperature input from user
-
 function convertTemp() {
+    // Get temperature input from user
     temp = tempInput.valueAsNumber;
+    
+    // Determine starting scale and ending scale and store in variable
     startingScaleValue = startingScale.options[startingScale.selectedIndex].value;
     endingScaleValue = endingScale.options[endingScale.selectedIndex].value;
+
+    if (startingScaleValue === 'celsius' && endingScaleValue === 'fahrenheit') {
+        console.log('converting cel to fah');
+    } else if (startingScaleValue === 'fahrenheit' && endingScaleValue === 'celsius') {
+        console.log('converting fah to cel');
+    } else if (startingScaleValue === 'celsius' && endingScaleValue === 'kelvin') {
+        console.log('converting cel to kelvin');
+    }
+
     console.log(temp);
     console.log(startingScaleValue);
     console.log(endingScaleValue);
 }
 
-// Determine starting scale and ending scale
-
-// Store in variable
-
 // C to F:
 // F = 9/5 * C + 32
+function cToF(temp) {
+    return result = ((9/5) * temp + 32);
+}
+
 
 // K to F:
 // F = 9/5 * (K - 273) + 32
