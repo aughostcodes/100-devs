f3008fd1103bfcfa18ce2cba0d4a4df9fd3016b0
August <hello@aughost.codes>
2022-02-28 16:04:10 -0500

    Temp converter functionality working

--- projects/js-assignments/temperature-converter/js/main.js
+++ projects/js-assignments/temperature-converter/js/main.js
@@ -1,9 +1,10 @@
 // Variables
 const tempInput = document.getElementById('temp-input');
-let temp;
 const convertButton = document.getElementById('submit-button');
 const startingScale = document.getElementById('starting-scale');
 const endingScale = document.getElementById('ending-scale');
+const display = document.querySelector('.display');
+let temp;
 let startingScaleValue;
 let endingScaleValue;
 let result;
@@ -19,12 +20,13 @@ function convertTemp() {
     startingScaleValue = startingScale.options[startingScale.selectedIndex].value;
     endingScaleValue = endingScale.options[endingScale.selectedIndex].value;
 
+    // Conditionals regarding which scales are selected
     if (startingScaleValue === 'celsius' && endingScaleValue === 'fahrenheit') {
-        console.log('converting cel to fah');
+        cToF(temp);
     } else if (startingScaleValue === 'fahrenheit' && endingScaleValue === 'celsius') {
-        console.log('converting fah to cel');
+        fToC(temp);
     } else if (startingScaleValue === 'celsius' && endingScaleValue === 'kelvin') {
-        console.log('converting cel to kelvin');
+        cToK(temp);
     }
 
     console.log(temp);
@@ -35,21 +37,41 @@ function convertTemp() {
 // C to F:
 // F = 9/5 * C + 32
 function cToF(temp) {
-    return result = ((9/5) * temp + 32);
+    result = ((9/5) * temp + 32).toFixed(2);
+    display.textContent = result;
 }
-
 
 // K to F:
 // F = 9/5 * (K - 273) + 32
+function kToF(temp) {
+    result = ((9/5) * (temp - 273) + 32).toFixed(2);
+    display.textContent = result;
+}
 
 // F to C:
 // C = 5/9 * (F - 32)
+function fToC(temp) {
+    result = ((5/9) * (temp - 32)).toFixed(2);
+    display.textContent = result;
+}
 
 // C to K:
 // K = C + 273
+function cToK(temp) {
+    result = (temp + 273).toFixed(2);
+    display.textContent = result;
+}
 
 // K to C:
 // C = K - 273
+function kToC(temp) {
+    result = (temp - 273).toFixed(2);
+    display.textContent = result;
+}
 
 // F to K:
 // K = 5/9 * (F - 32) + 273
+function fToK(temp) {
+    result = ((5/9) * (temp - 32) + 273).toFixed(2);
+    display.textContent = result;
+}
