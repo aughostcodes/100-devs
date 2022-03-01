570c522da21b2d11e6e4964d0b1c41f698862dae
August <hello@aughost.codes>
2022-03-01 01:49:31 -0500

    Logic for temp conv is correct?

--- projects/js-assignments/temperature-converter/js/main.js
+++ projects/js-assignments/temperature-converter/js/main.js
@@ -15,7 +15,7 @@ convertButton.addEventListener('click', convertTemp);
 function convertTemp() {
     // Get temperature input from user
     temp = tempInput.valueAsNumber;
-    
+
     // Determine starting scale and ending scale and store in variable
     startingScaleValue = startingScale.options[startingScale.selectedIndex].value;
     endingScaleValue = endingScale.options[endingScale.selectedIndex].value;
@@ -23,55 +23,63 @@ function convertTemp() {
     // Conditionals regarding which scales are selected
     if (startingScaleValue === 'celsius' && endingScaleValue === 'fahrenheit') {
         cToF(temp);
-    } else if (startingScaleValue === 'fahrenheit' && endingScaleValue === 'celsius') {
-        fToC(temp);
     } else if (startingScaleValue === 'celsius' && endingScaleValue === 'kelvin') {
         cToK(temp);
+    } else if (startingScaleValue === 'fahrenheit' && endingScaleValue === 'celsius') {
+        fToC(temp);
+    } else if (startingScaleValue === 'fahrenheit' && endingScaleValue === 'kelvin') {
+        fToK(temp);
+    } else if (startingScaleValue === 'kelvin' && endingScaleValue === 'celsius') {
+        kToC(temp);
+    } else if (startingScaleValue === 'kelvin' && endingScaleValue === 'fahrenheit') {
+        kToF(temp);
+    } else {
+        display.textContent = 'Please choose different scales to convert';
     }
 
-    console.log(temp);
-    console.log(startingScaleValue);
-    console.log(endingScaleValue);
-}
+    function displayResult() {
+        display.textContent = result;
+    }
 
-// C to F:
-// F = 9/5 * C + 32
-function cToF(temp) {
-    result = Math.round(((9/5) * temp + 32) * 100 / 100);
-    display.textContent = result;
-}
+    // C to F:
+    // F = 9/5 * C + 32
+    function cToF(temp) {
+        result = Math.round(((9 / 5) * temp + 32) * 100 / 100);
+        displayResult();
+    }
 
-// K to F:
-// F = 9/5 * (K - 273) + 32
-function kToF(temp) {
-    result = ((9/5) * (temp - 273) + 32).toFixed(2);
-    display.textContent = result;
-}
+    // K to F:
+    // F = 9/5 * (K - 273) + 32
+    function kToF(temp) {
+        result = ((9 / 5) * (temp - 273) + 32).toFixed(2);
+        displayResult();
+    }
 
-// F to C:
-// C = 5/9 * (F - 32)
-function fToC(temp) {
-    result = ((5/9) * (temp - 32)).toFixed(2);
-    display.textContent = result;
-}
+    // F to C:
+    // C = 5/9 * (F - 32)
+    function fToC(temp) {
+        result = ((5 / 9) * (temp - 32)).toFixed(2);
+        displayResult();
+    }
 
-// C to K:
-// K = C + 273
-function cToK(temp) {
-    result = (temp + 273).toFixed(2);
-    display.textContent = result;
-}
+    // C to K:
+    // K = C + 273
+    function cToK(temp) {
+        result = (temp + 273).toFixed(2);
+        displayResult();
+    }
 
-// K to C:
-// C = K - 273
-function kToC(temp) {
-    result = (temp - 273).toFixed(2);
-    display.textContent = result;
-}
+    // K to C:
+    // C = K - 273
+    function kToC(temp) {
+        result = (temp - 273).toFixed(2);
+        displayResult();
+    }
 
-// F to K:
-// K = 5/9 * (F - 32) + 273
-function fToK(temp) {
-    result = ((5/9) * (temp - 32) + 273).toFixed(2);
-    display.textContent = result;
+    // F to K:
+    // K = 5/9 * (F - 32) + 273
+    function fToK(temp) {
+        result = ((5 / 9) * (temp - 32) + 273).toFixed(2);
+        displayResult();
+    }
 }
