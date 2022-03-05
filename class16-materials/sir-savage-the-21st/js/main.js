f479920341b39c54778a7f3b27c5a0b043c5deff
August <hello@aughost.codes>
2022-03-05 04:27:36 -0500

    Class 16 savage 21 complete

--- class16-materials/sir-savage-the-21st/js/main.js
+++ class16-materials/sir-savage-the-21st/js/main.js
@@ -1,2 +1,21 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+function print21() {
+    for (let i = 0; i < 21; i++) {
+        console.log(21);
+    }
+}
+
+print21();
+
 //Bonus can you make it print '21' 21 times to the dom?
+let displayDiv = document.querySelector('#savageSays');
+
+function display21() {
+    for (let i = 0; i < 21; i++) {
+        let createdText = document.createElement('p');
+        createdText.textContent = '21';
+        displayDiv.appendChild(createdText);
+    }
+}
+
+display21();
