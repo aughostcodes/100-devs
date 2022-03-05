2adddbbbf3aafebac53b0f5b8694841d1ad1ec59
August <hello@aughost.codes>
2022-03-05 04:42:33 -0500

    Class 16 stop snacking complete

--- class16-materials/stop-snacking/js/main.js
+++ class16-materials/stop-snacking/js/main.js
@@ -1,4 +1,16 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
-function snackShit() {
-    let numberOfSnacks = document.querySelector('input');
+let input = document.querySelector('input');
+let h2Click = document.getElementById('help');
+let stopDisplay = document.getElementById('stops');
+
+h2Click.addEventListener('click', displayInputStops);
+
+function displayInputStops() {
+    // reset the display container if new input is entered without refresh
+    stopDisplay.textContent = '';
+    for (let i = 0; i < input.value; i++) {
+        let createdDiv = document.createElement('div');
+        createdDiv.textContent = 'Stop chugging malk!';
+        stopDisplay.appendChild(createdDiv);
+    }
 }
