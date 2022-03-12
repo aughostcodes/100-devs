9c9c89d6c38bb817b6500f641b6fed488907846b
August <hello@aughost.codes>
2022-03-12 04:46:32 -0500

    Coding JS warmup-1 up to makes10

--- projects/codingjs-exercises/main.js
+++ projects/codingjs-exercises/main.js
@@ -32,5 +32,16 @@ function diff21(n) {
     if (n < 21) {
         return 21 - n;
     }
-    return (21 - n) * 2;
+    return (n - 21) * 2;
+}
+
+/* parrotTrouble - We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble. */
+function parrotTrouble(talking, hour) {
+    return (talking && (hour < 7 || hour > 20));
+}
+
+/* makes10 - Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10. */
+// HAD A WEIRD BREAKTHROUGH RIGHT HERE. FOCUS ON LEARNING SIMPLE RETURN STATEMENTS USING BOOLEANS INSTEAD OF LARGE NESTED IF/ELSE LOOPS
+function makes10(a, b) {
+    return (a === 10 || b === 10) || (a + b === 10);
 }
