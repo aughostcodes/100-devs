6220f3014407af04b514b568320b5893e748edfb
August <hello@aughost.codes>
2022-03-16 11:56:14 -0400

    Fight to the finish edit

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -4,5 +4,11 @@ for (let i = 0; i < movies.length; i++) {
     document.querySelector('h2').innerText += movies[i];
 }
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
+let nums = [10, 20, 30];
+
+
+nums.forEach((item, index) => {
+    nums[i];
+})
 
 //Find the average of all the numbers from question three
