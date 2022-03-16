b495f69e98d157cc52482c606487a7186eef7cf5
August <hello@aughost.codes>
2022-03-16 11:55:42 -0400

    Fight to the finish edit

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,8 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
-
-
+let movies = ["fight", "win", "worldwide"];
+for (let i = 0; i < movies.length; i++) {
+    document.querySelector('h2').innerText += movies[i];
+}
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
 
 //Find the average of all the numbers from question three
