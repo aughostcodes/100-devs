1d9d3731ba2148919b7888d75904bb1ec12a38e4
August <hello@aughost.codes>
2022-03-17 21:07:46 -0400

    Class 20 materials

---
+++ class20-materials/review-in-paradise/js/main.js
@@ -0,0 +1,28 @@
+// *Variables*
+// Declare a variable, reassign it to your favorite food, and alert the value
+let soup = 'tofu soup';
+alert(soup);
+
+//Declare a variable, assign it a string, alert the second character in the string (Use your google-fu and the MDN)
+let secondChar = 'second';
+alert(secondChar[1]);
+
+// *Functions*
+// Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last. Alert the product. Call the function.
+function threeNums(num1, num2, num3) {
+    alert((num1 / num2) * num3);
+}
+
+threeNums(2, 3, 4);
+
+// Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
+function oneNum(num) {
+    console.log(Math.cbrt(num));
+}
+
+// *Conditionals*
+//Create a function that takes in a month. If it is a summer month alert "YAY". If another other month, alert "Booo"
+
+
+//*Loops*
+//Create a function that takes in a number. Console log every number from 1 to that number while skipping multiples of 5.
