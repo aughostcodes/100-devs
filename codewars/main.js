b6c05252a0d41a269f68ba262e2115d89c372454
August <hello@aughost.codes>
2022-03-20 14:02:11 -0400

    Working on sum of positive Codewars

--- codewars/main.js
+++ codewars/main.js
@@ -22,7 +22,6 @@ Your team is writing a fancy new text editor and you've been tasked with impleme
 Write a function which takes a list of strings and returns each line prepended by the correct number.
 The numbering starts at 1. The format is n: string. Notice the colon and space in between. */
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript //
-<<<<<<< HEAD
 const number = array =>
     array.map((val, idx) => `${idx + 1}: ${val}`);
 // SOLUTION
@@ -30,15 +29,28 @@ const number = array =>
 /* 8 kyu - Powers of 2
 Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ). */
 function powersOfTwo(n) {
-    return [];
+    let arr = [];
+    for (let i = 0; i <= n; i++) {
+        arr.push(2**i);
+    }
+    return arr;
 }
-=======
-function prependLines(arr) {
-    let result = [];
-    arr.forEach((element, index) => {
-        console.log(element);
-    });
-    console.log(result)
+// SOLVED
+
+
+/* 8 kyu - Sum of positive
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+Note: if there is nothing to sum, the sum is default to 0. */
+// https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+function positiveSum(arr) {
+    let positiveArr = [];
+    if (!arr) {
+        return 0;
+    } else {
+        positiveArr = arr.filter(num => num >= 0); 
+    }
+    let result = positiveArr.reduce((a, b) => a + b);
+    return result;
 }
 // IN PROGRESS
->>>>>>> e886dabfcc39158ab66d652191744850ba2dbf40
