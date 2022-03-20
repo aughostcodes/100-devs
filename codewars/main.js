152765fb52ffb5aeff1a75fd3fe731ed44644906
August <hello@aughost.codes>
2022-03-20 13:25:49 -0400

    Codewars 03/20/22

--- codewars/main.js
+++ codewars/main.js
@@ -11,7 +11,7 @@ function getVolumeOfCuboid(length, width, height) {
 /* 7 kyu - JavaScript Array Filter
 JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given. */
 function getEvenNumbers(numbersArray) {
-    return numbersArray.filter(function(n) {
+    return numbersArray.filter(function (n) {
         return n % 2 === 0;
     });
 }
@@ -22,9 +22,12 @@ Your team is writing a fancy new text editor and you've been tasked with impleme
 Write a function which takes a list of strings and returns each line prepended by the correct number.
 The numbering starts at 1. The format is n: string. Notice the colon and space in between. */
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript //
-let number = function(arr) {
-    for (let i = 1; i < arr; i++) {
-        return arr[i];
-    }
+const number = array =>
+    array.map((val, idx) => `${idx + 1}: ${val}`);
+// SOLUTION
+
+/* 8 kyu - Powers of 2
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ). */
+function powersOfTwo(n) {
+    return [];
 }
-// IN PROGRESS
