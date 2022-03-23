3a9cb6a55bbfe816eada7e48d075cc15e53b69cf
August <hello@aughost.codes>
2022-03-22 21:41:22 -0400

    Objects zeo class 21

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,16 @@
 //Create a stopwatch object that has four properties and three methods
+let stopWatch = {};
+
+stopWatch.currentTime = 9;
+stopWatch.startTime = 0;
+stopWatch.endTime = 99;
+stopWatch.bestTime = 420;
+
+stopWatch.start = function() {
+    console.log(`${stopWatch.startTime}`);
+}
+
+stopWatch.end = function() {
+    console.log(`Ending time at ${stopWatch.endTime}`);
+}
+
