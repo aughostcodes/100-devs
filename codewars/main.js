de986a6e04ee2e5d129bbc43f6e3f5d35f7a15e7
August <hello@aughost.codes>
2022-03-26 03:21:11 -0400

    Codewars 03/25/22

--- codewars/main.js
+++ codewars/main.js
@@ -31,7 +31,7 @@ Complete the function that takes a non-negative integer n as input, and returns 
 function powersOfTwo(n) {
     let arr = [];
     for (let i = 0; i <= n; i++) {
-        arr.push(2**i);
+        arr.push(2 ** i);
     }
     return arr;
 }
@@ -48,9 +48,27 @@ function positiveSum(arr) {
     if (arr.length === 0) {
         return 0;
     } else {
-        positiveArr = arr.filter(num => num >= 0); 
+        positiveArr = arr.filter(num => num >= 0);
     }
     let result = positiveArr.reduce((a, b) => a + b);
     return result;
 }
+// IN PROGRESS
+
+/* 6 kyu - Multiples of 3 or 5
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+Note: If the number is a multiple of both 3 and 5, only count it once. */
+// https://www.codewars.com/kata/514b92a657cdc65150000006/train
+function solution(num) {
+    let arr = [];
+    if (num) {
+        for (let i = 0; i < num; i++) {
+            // if (num[i] % 3 === 0 || num[i] % 5 === 0) {
+            //     arr.push(num[i]);
+            console.log(i);
+            }
+        }
+        // console.log(arr);
+    }
 // IN PROGRESS
