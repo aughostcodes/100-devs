363453b6ce96a5469a1bf12dd686636cb4c45ce2
August <hello@aughost.codes>
2022-03-30 19:06:12 -0400

    Coding JS front3, but Turbo CL

--- projects/codingjs-exercises/main.js
+++ projects/codingjs-exercises/main.js
@@ -88,5 +88,6 @@ function frontBack(str) {
 
 /* front3 - Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front. */
 function front3(str) {
-    console.log(str.startsWith('', 3));
+    str = str.split('');
+    let frontString = '';
 }
