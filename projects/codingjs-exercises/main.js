aa64ae6b4bdbd984239e34f92fa7343ca7c9e1d8
August <hello@aughost.codes>
2022-04-06 22:44:40 -0400

    Coding JS frontTimes started

--- projects/codingjs-exercises/main.js
+++ projects/codingjs-exercises/main.js
@@ -318,9 +318,20 @@ function everyNth(str, n) {
 }
 // Got bored. Will come back here, keep moving
 
+
 // WARMUP-2
 
 /* stringTimes - Given a string and a non-negative int n, return a larger string that is n copies of the original string. */
 function stringTimes(str, n) {
-    
+    let copiedStr = '';
+    for (let i = 0; i < n; i++) {
+        copiedStr += str;
+    }
+    return copiedStr;
+}
+// First try, and quickly as well! :)
+
+/* frontTimes - Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front. */
+function frontTimes(str, n) {
+
 }
