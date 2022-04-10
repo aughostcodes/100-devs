59ab555bda3ea1dc571877436d3804aad5f3df3f
August <hello@aughost.codes>
2022-04-10 16:00:02 -0400

    RPDR API, added for loop to display appearances in multiple seasons

--- projects/api-testing/rpdr/main.js
+++ projects/api-testing/rpdr/main.js
@@ -10,15 +10,24 @@ const famousQuote = document.querySelector('.quote');
 const seasons = document.querySelector('.seasons');
 const queenPic = document.querySelector('.queen-pic');
 
-fetch(`http://www.nokeynoshade.party/api/queens/${createRandom()}`)
+fetch(`http://www.nokeynoshade.party/api/queens/34`)
     .then(res => res.json())
     .then(data => {
         dragName.textContent = data.name;
         famousQuote.textContent = data.quote;
-        // seasons.textContent = seasons[0].seasonNumber;
+        if (data.seasons.length > 1) {
+            for (let i = 0; i < data.seasons.length; i++) {
+                console.log(`${data.seasons[i].seasonNumber}`);
+                let newDiv = document.createAttribute('div');
+                newDiv.value = data.seasons[i].seasonNumber;
+                seasons.setAttributeNode(newDiv);
+            }
+        }
+        // seasons.textContent = `Featured in season ${data.seasons[0].seasonNumber}`;
         queenPic.src = data.image_url;
         console.log(data);
-        // console.log('seasons[0]: ', data.seasons[0]);
+        console.log('seasons: ', data.seasons.length);
+        console.log(data.seasons[0].seasonNumber)
     })
     .catch(err => {
         console.log(`error: ${err}`);
