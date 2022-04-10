f7b86bc1f888704a514be6f5db68cc69ab47698d
August <hello@aughost.codes>
2022-04-10 01:21:12 -0400

    RPDR API stuff

--- projects/api-testing/rpdr/main.js
+++ projects/api-testing/rpdr/main.js
@@ -7,12 +7,18 @@ const createRandom = () => {
 }
 const dragName = document.querySelector('.drag-name');
 const famousQuote = document.querySelector('.quote');
+const seasons = document.querySelector('.seasons');
+const queenPic = document.querySelector('.queen-pic');
 
-fetch(`http://www.nokeynoshade.party/api/queens/all`)
+fetch(`http://www.nokeynoshade.party/api/queens/${createRandom()}`)
     .then(res => res.json())
     .then(data => {
-        console.log([createRandom].id);
+        dragName.textContent = data.name;
+        famousQuote.textContent = data.quote;
+        // seasons.textContent = seasons[0].seasonNumber;
+        queenPic.src = data.image_url;
         console.log(data);
+        // console.log('seasons[0]: ', data.seasons[0]);
     })
     .catch(err => {
         console.log(`error: ${err}`);
