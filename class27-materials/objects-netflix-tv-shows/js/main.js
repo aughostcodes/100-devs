840da9658c8a7d94cd20addf222b488b1d151c83
August <hello@aughost.codes>
2022-04-14 13:06:29 -0400

    Class 27 Netflix show class exercise

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,14 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
 
+class smallScreenShow {
+    constructor (makeTitle, makeGuest, makeThemeSong, makeJoke) {
+        this.title = makeTitle;
+        this.guest = makeGuest;
+        this.theme = makeThemeSong;
+        this.joke = makeJoke;
+    }
+    shoutName () {
+        console.log('THIS IS THE NAME!');
+    }
+}
