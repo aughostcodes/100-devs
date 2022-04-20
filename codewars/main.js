b611464d79163aa51c2da69f9de5d8bf92ee3acf
August <hello@aughost.codes>
2022-04-20 00:31:19 -0400

    Codewars 8 kyus, 9 so far today

--- codewars/main.js
+++ codewars/main.js
@@ -37,24 +37,6 @@ function powersOfTwo(n) {
 }
 // SOLVED
 
-
-/* 8 kyu - Sum of positive
-You get an array of numbers, return the sum of all of the positives ones.
-Example [1,-4,7,12] => 1 + 7 + 12 = 20
-Note: if there is nothing to sum, the sum is default to 0. */
-// https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
-function positiveSum(arr) {
-    let positiveArr = [];
-    if (arr.length === 0) {
-        return 0;
-    } else {
-        positiveArr = arr.filter(num => num >= 0);
-    }
-    let result = positiveArr.reduce((a, b) => a + b);
-    return result;
-}
-// IN PROGRESS
-
 /* 6 kyu - Multiples of 3 or 5
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
@@ -98,7 +80,89 @@ function strEndsWith(str, ending) {
 Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. */
 function countSheep(num) {
     let result = '';
-    for(let i = 1; i <= num; i++) { result+= `${i} sheep...`; }
+    for (let i = 1; i <= num; i++) {
+        result += `${i} sheep...`;
+    }
     return result;
 }
 // SOLUTION; Mine got convoluted but did pass most of the tests
+
+/* 8 kyu - Opposite number
+Very simple, given an integer or a floating-point number, find its opposite. */
+function opposite(number) {
+    return number - (number * 2);
+}
+// SOLVED; SO'd this great answer while looking for Math.abs() info. Submitted, and top answer is simply return(-number) :/
+
+/* 8 kyu - Convert a Number to a String!
+We need a function that can transform a number into a string. */
+function numberToString(num) {
+    return num.toString();
+}
+// SOLVED
+
+/* 8 kyu - Convert boolean values to strings 'Yes' or 'No'.
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. */
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+}
+// SOLVED
+
+/* 8 kyu - Reversed Strings
+Complete the solution so that it reverses the string passed into it. */
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+// SOLVED; Quickly, as well
+
+/* 8 kyu - Sum of positive
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+Note: if there is nothing to sum, the sum is default to 0. */
+function positiveSum(arr) {
+    let newArr = arr.filter(num => num >= 0)
+        .reduce(((a, b) => a + b), 0);
+    return newArr;
+}
+// SOLVED; That 0 initialValue at the end of reduce there made all the difference in passing tests
+
+/* 8 kyu - Return Negative
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative? */
+function makeNegative(num) {
+    return -Math.abs(num);
+}
+// SOLVED; Not sure why this one required Math.abs, but it was returning -0 using -(num)
+
+/* 8 kyu - Find the smallest integer in the array
+Given an array of integers your solution should find the smallest integer.
+For example:
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty. */
+function findSmallestInt(args) {
+    let newArgs = args.sort((a, b) => a - b);
+    return newArgs[0];
+}
+// SOLVED; Check out Int32Array? 
+// let newArgs = new Int32Array(args);
+// Damn, Math.min(...args). Oh well, got in some sort() practice
+
+/* 8 kyu - Grasshopper-Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. */
+let summation = function (num) {
+    let total = 0;
+    for (let i = 1; i <= num; i++) {
+        total += i;
+    }
+    return total;
+}
+
+/* 8 kyu - Square(n) Sum
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9. */
+function squareSum(numbers) {
+    let newArr = numbers.map(num => Math.pow(num, 2))
+        .reduce(((a, b) => a + b), 0);
+    return newArr;
+}
+// SOLVED; That trailing 0 in reduce again... Also, tried forEach here first
