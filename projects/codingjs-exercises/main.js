56b414e9bbe4c003843d241a600ffb44b5717c4a
August <hello@aughost.codes>
2022-04-20 04:38:41 -0400

    Coding JS exercises, last2 in the works

--- projects/codingjs-exercises/main.js
+++ projects/codingjs-exercises/main.js
@@ -406,3 +406,18 @@ function stringSplosion(str) {
     console.log('anotherString: ', anotherString);
 }
 // Confused here, getting close but not really
+
+/* last2 - Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring). */
+function last2(str) {
+    let count = 0;
+    let clip = str.substring(str.length - 2);
+    console.log('clip: ', clip);
+    if ((str.indexOf(clip) !== str.length - 2) && (str.indexOf(clip))) {
+        for (let i = 0; i < str.length - 2; i++) {
+            console.log(str.indexOf(clip))
+        }
+        count++;
+    }
+    // return count;
+}
+// I tried, too tired. It's 4:37AM
