e188528a4970c31d4527d740f56670896de0c26f
August <hello@aughost.codes>
2022-04-26 23:02:31 -0400

    I don't know, honestly

--- class29-materials/cascade-badge/js/main.js
+++ class29-materials/cascade-badge/js/main.js
@@ -1,6 +1,9 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
-
+let monList = ['Bulbasaur', 'Weedle', 'Magneton', 'Victreebell'];
+function reverseMyMons() {
+    return monList.reverse();
+}
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
