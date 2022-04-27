9b2c687c1aa5faf3d5c0062d3fff89b0b31b22ee
August <hello@aughost.codes>
2022-04-26 23:06:03 -0400

    Oh lord idk

--- class29-materials/cascade-badge/js/main.js
+++ class29-materials/cascade-badge/js/main.js
@@ -5,10 +5,13 @@ function reverseMyMons() {
     return monList.reverse();
 }
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+let a = [1, 2, 3];
+let b = [4, 5, 6];
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
+
 
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
