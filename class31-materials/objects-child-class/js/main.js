6e715e95452f7562ff29fbc823ed497a9b15d560
August <hello@aughost.codes>
2022-05-03 21:54:46 -0400

    Class 31 regarding OOP was very informative; practice more with classes

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,144 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+// //Create a class and extend it - Can be anything you would like it to be! 
 
+// class Animal {
+//     constructor(name, sound) {
+//         this.name = name;
+//         this.sound = sound;
+//     }
+//     speak() {
+//         console.log(`${this.name} says ${this.sound}!`);
+//     }
+// }
+
+// class Dog extends Animal {
+//     constructor(name, breed) {
+//         super(name);
+//         this.breed = breed;
+//         this.sound = `woof`;
+//     }
+// }
+
+// class Cat extends Animal {
+//     constructor(name, color) {
+//         super(name);
+//         this.color = color;
+//         this.sound = `meow`;
+//     }
+// }
+
+// class Moogle extends Animal {
+//     constructor(name, game) {
+//         super(name);
+//         this.game = game;
+//         this.sound = 'kupo';
+//     }
+// }
+
+// class Chicken extends Animal {
+//     constructor(name, food, type) {
+//         super(name);
+//         this.food = food;
+//         this.sound = `bawk`;
+//         this.type = type;
+//     }
+//     bummer() {
+//         console.log(`Aw, ${this.name} is now ${this.type}.`)
+//     }
+// }
+
+// let honey = new Dog('Honey', 'perfect mutt');
+// console.log('honey: ', honey);
+// console.log(honey.speak());
+
+// let jonesy = new Cat('Jonesy', 'orange');
+// console.log('jonesy: ', jonesy);
+// console.log(jonesy.speak());
+
+// let stiltzkin = new Moogle('Stiltzkin', 'Final Fantasy IX');
+// console.log('stiltzkin: ', stiltzkin);
+// console.log(stiltzkin.speak());
+
+// let tyson = new Chicken('Tyson', 'corn and crickets', 'strips');
+// console.log('tyson: ', tyson);
+// console.log(tyson.speak());
+// console.log(tyson.bummer());
+// console.log(tyson.food);
+
+
+class Animal {
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    speak() {
+        console.log(`${this._name} makes a sound.`);
+    }
+}
+
+class Turtle extends Animal {
+    constructor(name, hatColor) {
+        super(name);
+        this._hatColor = hatColor;
+    }
+
+    get hatColor() {
+        return this._hatColor;
+    }
+
+    speak() {
+        console.log(`${this.name} isn't great at speaking.`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this._breed = breed;
+    }
+
+    get breed() {
+        return this._breed;
+    }
+
+    speak() {
+        super.speak();
+        console.log(`${this.name} barks!`);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this._breed = breed;
+    }
+
+    get breed() {
+        return this._breed;
+    }
+
+    speak() {
+        super.speak();
+        console.log(`${this.name} meows!`);
+    }
+}
+
+const franklin = new Turtle('Franklin', 'red');
+console.log('franklin ', franklin);
+
+const kalea = new Dog('Kalea', 'mutt');
+console.log('kalea: ', kalea);
+console.log(kalea.speak());
+
+const desmond = new Cat ('Desmond', 'meww');
+console.log(desmond.speak());
+
+let farm = [franklin, kalea, desmond];
+console.log('farm: ', farm);
+
+for (i of farm) {
+    console.log(i.speak());
+}
