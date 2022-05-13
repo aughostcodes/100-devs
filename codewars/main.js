66fb583fa77d7075b7d89d4ca49d47a6b0b57df2
August <hello@aughost.codes>
2022-05-12 22:16:54 -0400

    7 kyu complete

--- codewars/main.js
+++ codewars/main.js
@@ -615,3 +615,24 @@ function dontGiveMeFive(start, end) {
 //     }
 //     return res.length;
 // }
+
+/* 7 kyu - Odd or Even?
+Given a list of integers, determine whether the sum of its elements is odd or even.
+Give your answer as a string matching "odd" or "even".
+If the input array is empty consider it as: [0] (array with a zero).
+Examples:
+Input: [0]
+Output: "even"
+Input: [0, 1, 4]
+Output: "odd"
+Input: [0, -1, -5]
+Output: "even" */
+function oddOrEven(array) {
+    if (array.length === 0) {
+        return 'even';
+    } else {
+        const resultArr = array.reduce((a, b) => a + b);
+        return Math.abs(resultArr) % 2 === 0 ? 'even' : 'odd';
+    }
+}
+// SOLVED; Top result: return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
