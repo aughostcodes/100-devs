9d9c35e614a8e0b7c55b19ef30b692e886d17281
August <hello@aughost.codes>
2022-05-19 20:52:26 -0400

    Adding cohort-organizer for the last time...

---
+++ projects/cohort-organizer/js/main.js
@@ -0,0 +1,161 @@
+/* =======================
+##########################
+    DOM SELECTORS
+##########################
+======================= */
+
+let classContainer = document.querySelector('.class-container');
+
+let classNumberText = document.querySelector('.class-number');
+let classDateText = document.querySelector('.class-date');
+let classVideo = document.querySelector('.class-video');
+let classVideoUrl = document.querySelector('#class-video-url');
+let classVideoInfo = document.querySelector('.class-video-info');
+let classMaterials = document.querySelector('.class-materials');
+let classMaterialsUrl = document.querySelector('#class-materials-url');
+let classSolutions = document.querySelector('.class-solutions');
+let classSolutionsUrl = document.querySelector('#class-solutions-url');
+let classSlidesUrl = document.querySelector('#class-slides-url');
+let classMotivation = document.querySelector('.class-motivation');
+let classMotivationUrl = document.querySelector('#class-motivation-url');
+let classCheckin = document.querySelector('.class-checkin');
+let classCheckinUrl = document.querySelector('#class-checkin-url');
+
+
+/* =======================
+##########################
+    CONSTRUCTORS
+##########################
+======================= */
+
+function createNewEntryInDom(classNum) {
+    let classNumVariable = classNum;
+    let originalEntry = document.querySelector('.original-details');
+    let newEntryDisplay = originalEntry.cloneNode(true);
+    console.log('newEntryDisplay: ', newEntryDisplay);
+    classContainer.appendChild(newEntryDisplay);
+}
+
+// newDetailEntry.classList.add('class-details');
+// classContainer.appendChild(newDetailEntry);
+// newDetailEntry.appendChild(classNumberText);
+
+class NewClassEntry {
+    constructor(number, date, videoLink, videoThumbnail, materials, solutions, slides, motivation, checkin) {
+        this.number = number;
+        this.date = date;
+        this.videoLink = videoLink;
+        this.videoThumbnail = videoThumbnail;
+        this.materials = materials;
+        this.solutions = solutions;
+        this.slides = slides;
+        this.motivation = motivation;
+        this.checkin = checkin;
+    }
+
+    changeClassNumber() {
+        return classNumberText.textContent = this.number;
+    }
+
+    changeDate() {
+        return classDateText.textContent = this.date;
+    }
+
+    changeVideoLink() {
+        classVideoUrl.setAttribute('href', `${this.videoLink}`);
+        classVideoInfo.setAttribute('title', `Class ${this.number} Video`);
+        classVideoInfo.setAttribute('alt', `Class ${this.number} Video`);
+        classVideoInfo.setAttribute('poster', `${this.videoThumbnail}`);
+    }
+
+    changeMaterialsLink() {
+        if (!this.materials) {
+            classMaterials.style.display = 'none';
+        }
+        return classMaterialsUrl.setAttribute('href', `${this.materials}`);
+    }
+
+    changeSolutionsLink() {
+        if (!this.solutions) {
+            classSolutions.style.display = 'none';
+        }
+        return classSolutionsUrl.setAttribute('href', `${this.solutions}`);
+    }
+
+    changeSlidesLink() {
+        classSlidesUrl.setAttribute('href', `${this.slides}`);
+        return classNumberText.textContent = this.number;
+    }
+
+    changeMotivationLink() {
+        if (!this.motivation) {
+            classMotivation.style.display = 'none';
+        }
+        return classMotivationUrl.setAttribute('href', `${this.motivation}`);
+    }
+
+    changeCheckinLink() {
+        if (!this.checkin) {
+            classCheckin.style.display = 'none';
+        }
+        return classCheckinUrl.setAttribute('href', `${this.checkin}`)
+    }
+
+    runAllCreateEntryFunctions() {
+        this.changeClassNumber();
+        this.changeDate();
+        this.changeVideoLink();
+        this.changeMaterialsLink();
+        this.changeSolutionsLink();
+        this.changeSlidesLink();
+        this.changeMotivationLink();
+        this.changeCheckinLink();
+    };
+}
+
+// class Homework extends NewClassEntry {
+//     constructor(number, watch, homeworkDue, submissionLink) {
+//         super(number);
+//         this.watch = watch;
+//         this.homeworkDue = homeworkDue;
+//         this.submissionLink = submissionLink;
+//     }
+// }
+
+
+/* =======================
+##########################
+CREATING EACH CLASS ENTRY
+##########################
+======================= */
+
+// `https://www.poetryfoundation.org/poems/51642/invictus`
+
+const class01 = new NewClassEntry(
+    `01`,
+    `Jan 11, 2022`,
+    `https://www.youtube.com/watch?v=L--DQgd1JVM`,
+    `http://img.youtube.com/vi/L--DQgd1JVM/hqdefault.jpg`,
+    `https://cdn.discordapp.com/attachments/738891289071714388/930589829421473862/class01-materials.zip`,
+    null,
+    `https://slides.com/leonnoel/100devs2-html-the-basics`,
+    `https://www.poetryfoundation.org/poems/51642/invictus`,
+    `https://twitter.com/leonnoel/status/1481030723347746816`);
+
+// createNewEntryInDom();
+class01.runAllCreateEntryFunctions();
+
+const class02 = new NewClassEntry(
+    `02`,
+    `Jan 13, 2022`,
+    `https://www.youtube.com/watch?v=L--DQgd1JVM`,
+    `http://img.youtube.com/vi/sRv19gkZ4E0/hqdefault.jpg`,
+    null,
+    null,
+    `https://slides.com/leonnoel/100devs2-html-the-basics`,
+    null,
+    `https://twitter.com/leonnoel/status/1481030723347746816`);
+
+
+createNewEntryInDom(02);    
+class02.runAllCreateEntryFunctions();
