9f97952d800e692eb9bd02a32164d274b4a2d6c2
August <hello@aughost.codes>
2022-05-27 08:54:14 -0400

    Well, thought I'd start a kata but time for work

--- codewars/main.js
+++ codewars/main.js
@@ -636,3 +636,26 @@ function oddOrEven(array) {
     }
 }
 // SOLVED; Top result: return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+
+/* 7 kyu - Two fighters, one winner
+Create a function that returns the name of the winner in a fight between two fighters.
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+Example:
+declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+Lew attacks Harry; Harry now has 3 health.
+Harry attacks Lew; Lew now has 6 health.
+Lew attacks Harry; Harry now has 1 health.
+Harry attacks Lew; Lew now has 2 health.
+Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+*/
+function Fighter(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function() { return this.name; }
+}
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    
+}
