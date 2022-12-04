38e97014ac1efffaaedb102d95c9fa5b2d264439
August <hello@aughost.codes>
2022-12-04 13:27:06 -0500

    Added search bar for finding specific Pokemon

--- projects/api-testing/pokeapi/main.js
+++ projects/api-testing/pokeapi/main.js
@@ -3,7 +3,9 @@
 // https://pokeapi.co/docs/v2#resource-listspagination-section
 
 // Variables
+const searchBar = document.querySelector('#query');
 const goButton = document.querySelector('.go-button');
+const searchButton = document.querySelector('.search-button');
 const pokeName = document.querySelector('.poke-name');
 let pokeType1 = document.querySelector('.poke-type1');
 let pokeType2 = document.querySelector('.poke-type2');
@@ -12,6 +14,13 @@ const frontSprite = document.querySelector('.front-sprite');
 // Create random number to find random mon
 const createRandom = () => { 
     return Math.floor(Math.random() * 905) + 1; // 1126? How many mons are there, actually?
+}
+
+const grabTextFromSearch = () => {
+    let query = searchBar.value;
+    let regex = /'/g;
+    let result = query.replace(regex, '');
+    return result;
 }
 
 let fetchTheMon = () => fetch(`https://pokeapi.co/api/v2/pokemon/${createRandom()}`)
@@ -29,6 +38,20 @@ let fetchTheMon = () => fetch(`https://pokeapi.co/api/v2/pokemon/${createRandom(
     console.log(`error ${err}`);
 });
 
+let searchForPoke = () => fetch(`https://pokeapi.co/api/v2/pokemon/${grabTextFromSearch()}`)
+.then(res => res.json())
+.then(data => {
+    pokeName.textContent = data.name.toUpperCase();
+    frontSprite.src = data.sprites.front_default;
+    pokeType1.textContent = data.types[0].type.name;
+    pokeType2.textContent = data.types[1].type.name;
+
+    console.log(data);
+})
+.catch(err => {
+    console.log(`error ${err}`);
+});
 
 
 goButton.addEventListener('click', fetchTheMon);
+searchButton.addEventListener('click', searchForPoke);
